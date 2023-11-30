917a258b4776d25217ec6355cfcc271660f45f42
SinghALGO <karanssandhu92@gmail.com>
2023-11-30 14:06:56 -0800

    Added analytics requirement to track all visits to url

--- express_server.js
+++ express_server.js
@@ -30,18 +30,10 @@ const urlDatabase = {};
 
 const users = {};
 
+let visits = {};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/urls", (req, res) => {
-  //Calling function urlsForUser to get refined urlDatabse containing only the shortURLs that were created by the user
-  const refinedUrlsDatabase = urlsForUser(req.session.user_id, urlDatabase);
-  const templateVars = {
-    user: users[req.session.user_id],
-    urls: refinedUrlsDatabase,
-  };
-  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -54,6 +46,16 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+app.get("/register", (req, res) => {
+  //Checking if user is logged-in
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = { user: users[req.session.user_id] };
+    res.render("user_registration", templateVars);
+  }
+});
+
 app.get("/urls/:id", (req, res) => {
   //Checking if user is logged-in
   if (req.session.user_id) {
@@ -61,11 +63,16 @@ app.get("/urls/:id", (req, res) => {
     if (urlDatabase[req.params.id]) {
       //Checking if the id(short URL) belongs to the user and was created by him
       if (urlDatabase[req.params.id].userID === req.session.user_id) {
+        let totalvisits;
+        visits[req.params.id]
+          ? (totalvisits = visits[req.params.id])
+          : (totalvisits = 0);
         const longURL = urlDatabase[req.params.id].longURL;
         const templateVars = {
           user: users[req.session.user_id],
           id: req.params.id,
           longURL,
+          visits: totalvisits,
         };
         res.render("urls_show", templateVars);
       } else {
@@ -76,6 +83,41 @@ app.get("/urls/:id", (req, res) => {
     }
   } else {
     res.send("Please register/login first");
+  }
+});
+
+app.get("/urls", (req, res) => {
+  //Calling function urlsForUser to get refined urlDatabse containing only the shortURLs that were created by the user
+  const refinedUrlsDatabase = urlsForUser(req.session.user_id, urlDatabase);
+  const templateVars = {
+    user: users[req.session.user_id],
+    urls: refinedUrlsDatabase,
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  //Checking if it is a valid id(short URL)
+  if (urlDatabase[req.params.id]) {
+    const longURL = urlDatabase[req.params.id].longURL;
+    if (visits[req.params.id]) {
+      visits[req.params.id] = visits[req.params.id] + 1;
+    } else {
+      visits[req.params.id] = 1;
+    }
+    res.redirect(longURL);
+  } else {
+    res.send("<h2>This short url does not exist.</h2>");
+  }
+});
+
+app.get("/login", (req, res) => {
+  //Checking if the user is already logged in
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = { user: users[req.session.user_id] };
+    res.render("urls_login", templateVars);
   }
 });
 
@@ -94,37 +136,44 @@ app.post("/urls", (req, res) => {
   }
 });
 
-app.get("/u/:id", (req, res) => {
-  //Checking if it is a valid id(short URL)
-  if (urlDatabase[req.params.id]) {
-    const longURL = urlDatabase[req.params.id].longURL;
-    res.redirect(longURL);
+app.post("/register", (req, res) => {
+  //Checking if entered email or password is not empty
+  if (req.body.email === "" || req.body.password === "") {
+    res.sendStatus(400);
+  }
+  //Checking if user with same email has already registered by calling findUserbyEmail function
+  else if (findUserByEmail(req.body.email, users)) {
+    res.sendStatus(400);
   } else {
-    res.send("<h2>This short url does not exist.</h2>");
+    const id = generateRandomString();
+    const password = req.body.password;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    users[id] = { id: id, email: req.body.email, password: hashedPassword };
+    req.session.user_id = id;
+    res.redirect("/urls");
   }
 });
 
-app.delete("/urls/:id/delete", (req, res) => {
-  //Checking if user is logged in
-  if (req.session.user_id) {
-    //Checking if the id(short URL exists in urlDatabase)
-    if (urlDatabase[req.params.id]) {
-      //Checking if the id(short URL) belongs to the user and was created by him
-      if (urlDatabase[req.params.id].userID === req.session.user_id) {
-        const id = req.params.id;
-        delete urlDatabase[id];
-        res.redirect("/urls");
-      } else {
-        res.send(
-          "You can not delete it as this short URL is not accessible to you."
-        );
-      }
-    } else {
-      res.send("Wrong short URL id ");
-    }
+app.post("/login", (req, res) => {
+  //Calling findUserbyEmail function and using it's return value in "if" statement. If user is registered then only let him login
+  const userFinder = findUserByEmail(req.body.email, users);
+  if (!userFinder) {
+    res.sendStatus(403);
   } else {
-    res.send("Please register/login first");
+    //Comparing passwords entering in login form and password entered while registering
+    if (bcrypt.compareSync(req.body.password, userFinder.password)) {
+      req.session.user_id = userFinder.id;
+      res.redirect("urls");
+    } else {
+      res.sendStatus(403);
+    }
   }
+});
+
+app.post("/logout", (req, res) => {
+  //Clearing cookies
+  req.session = null;
+  res.redirect("/login");
 });
 
 app.put("/urls/:id", (req, res) => {
@@ -154,64 +203,26 @@ app.put("/urls/:id", (req, res) => {
   }
 });
 
-app.get("/register", (req, res) => {
-  //Checking if user is logged-in
+app.delete("/urls/:id/delete", (req, res) => {
+  //Checking if user is logged in
   if (req.session.user_id) {
-    res.redirect("/urls");
-  } else {
-    const templateVars = { user: users[req.session.user_id] };
-    res.render("user_registration", templateVars);
-  }
-});
-
-app.post("/logout", (req, res) => {
-  //Clearing cookies
-  res.clearCookie("session");
-  res.clearCookie("session.sig");
-  res.redirect("/login");
-});
-
-app.post("/register", (req, res) => {
-  //Checking if entered email or password is not empty
-  if (req.body.email === "" || req.body.password === "") {
-    res.sendStatus(400);
-  }
-  //Checking if user with same email has already registered by calling findUserbyEmail function
-  else if (findUserByEmail(req.body.email, users)) {
-    res.sendStatus(400);
-  } else {
-    const id = generateRandomString();
-    const password = req.body.password;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    users[id] = { id: id, email: req.body.email, password: hashedPassword };
-    req.session.user_id = id;
-    res.redirect("/urls");
-  }
-});
-
-app.get("/login", (req, res) => {
-  //Checking if the user is already logged in
-  if (req.session.user_id) {
-    res.redirect("/urls");
-  } else {
-    const templateVars = { user: users[req.session.user_id] };
-    res.render("urls_login", templateVars);
-  }
-});
-
-app.post("/login", (req, res) => {
-  //Calling findUserbyEmail function and using it's return value in "if" statement. If user is registered then only let him login
-  const userFinder = findUserByEmail(req.body.email, users);
-  if (!userFinder) {
-    res.sendStatus(403);
-  } else {
-    //Comparing passwords entering in login form and password entered while registering
-    if (bcrypt.compareSync(req.body.password, userFinder.password)) {
-      req.session.user_id = userFinder.id;
-      res.redirect("urls");
+    //Checking if the id(short URL exists in urlDatabase)
+    if (urlDatabase[req.params.id]) {
+      //Checking if the id(short URL) belongs to the user and was created by him
+      if (urlDatabase[req.params.id].userID === req.session.user_id) {
+        const id = req.params.id;
+        delete urlDatabase[id];
+        res.redirect("/urls");
+      } else {
+        res.send(
+          "You can not delete it as this short URL is not accessible to you."
+        );
+      }
     } else {
-      res.sendStatus(403);
+      res.send("Wrong short URL id ");
     }
+  } else {
+    res.send("Please register/login first");
   }
 });
 
